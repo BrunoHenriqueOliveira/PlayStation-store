e4072d1736946ca1d73c0fcb390a3174450d007a
BrunoHenriqueOliveira <bruno.oliveira.tc@outlook.com>
2025-03-03 17:46:36 -0300

    refactor: repassando parametros de componentes pais para filhos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {CardLabelComponent} from './card-label/card-label.component';
 import {CardPricingComponent} from './card-pricing/card-pricing.component';
 
@@ -13,4 +13,12 @@ import {CardPricingComponent} from './card-pricing/card-pricing.component';
 })
 export class CardComponent {
 
+	@Input()
+	gameCover:string = "";
+	@Input()
+	gameLabel:string = "";
+	@Input()
+	gameType:string = "XPTO | PS4";
+	@Input()
+	gamePrice:string = "R$ 399,90";
 }
